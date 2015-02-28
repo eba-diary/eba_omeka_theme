e934a601d98c488500198af0a1f6eb0b10653e81
Chris Sumption <fridaysedge@gmail.com>
2015-02-28 09:56:45 -0800

    Added links and tititle content to the Supporter images.

--- common/footer.php
+++ common/footer.php
@@ -4,10 +4,10 @@
 
     <div id="footer-content" class="center-div">
         <div id="supporters">
-            <img src="<?php echo img('uwch_logo2.png'); ?>" />
-            <img src="<?php echo img('neh_logo.jpg'); ?>" />
-            <img src="<?php echo img('NELC_icon.jpg'); ?>" />
-            <img src="<?php echo img('ndt-logo.png'); ?>" />
+            <a href="http://simpsoncenter.org/" title="Simpson Center For The Humanities"><img src="<?php echo img('uwch_logo2.png'); ?>" /></a>
+            <a href="http://www.neh.gov/" title="National Endowment For The Humanities"><img src="<?php echo img('neh_logo.jpg'); ?>" /></a>
+            <a href="http://depts.washington.edu/nelc/" title="Department of Near Eastern Languages and Civilization"><img src="<?php echo img('NELC_icon.jpg'); ?>" /></a>
+            <a href="http://depts.washington.edu/ndth/" title="Newbook Digital Texts"><img src="<?php echo img('ndt-logo.png'); ?>" /></a>
         </div>
         <?php if($footerText = get_theme_option('Footer Text')): ?>
         <div id="custom-footer-text">
